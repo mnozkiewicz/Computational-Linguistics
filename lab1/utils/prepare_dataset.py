9b94c2f341a5165953c43a7138f5e34595c0eecd
mnozkiewicz <michno513@gmail.com>
2025-10-25 21:08:40 +0200

    Change training dataset

--- lab1/utils/prepare_dataset.py
+++ lab1/utils/prepare_dataset.py
@@ -46,10 +46,10 @@ def prepare_dataset(
 
 
 def main():
-    sl = Speakleash("./speakleash_data")
+    sl = Speakleash("../speakleash_data")
 
-    prepare_dataset(sl, "wolne_lektury_corpus", "data/train/", 50_000_000, 10)
-    prepare_dataset(sl, "plwiki", "data/test/", 1_000_000)
+    prepare_dataset(sl, "news_5_lifestyle_corpus", "../data/train/", 50_000_000, 10)
+    prepare_dataset(sl, "plwiki", "../data/test/", 1_000_000)
 
 
 if __name__ == '__main__':
