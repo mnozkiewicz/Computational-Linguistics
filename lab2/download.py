ae61e7822a42e5e1f3e0b836b7368f8243a0f740
mnozkiewicz <michno513@gmail.com>
2025-11-12 19:00:22 +0100

    Started lab2

---
+++ lab2/download.py
@@ -0,0 +1,9 @@
+# from datasets import load_dataset
+
+# ds = load_dataset("roneneldan/TinyStories")
+# ds.save_to_disk("data/tinystories")
+# from datasets import load_from_disk
+
+# def load_dataset():
+#     print("Loading")
+#     return load_from_disk("data/tinystories")
