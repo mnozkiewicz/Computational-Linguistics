d5fdff9ab0af3531800712c2f1767ff662d9dcb3
mnozkiewicz <michno513@gmail.com>
2025-11-15 22:11:08 +0100

    Movel evaluation

--- lab2/utils/download_dataset.py
+++ lab2/utils/download_dataset.py
@@ -1,5 +1,8 @@
 from datasets import load_dataset
 
 if __name__ == "__main__":
-    ds = load_dataset("roneneldan/TinyStories")
-    ds.save_to_disk("data/tinystories")
+    # ds = load_dataset("roneneldan/TinyStories")
+    # ds.save_to_disk("data/tinystories")
+
+    ds = load_dataset("wikitext", "wikitext-2-raw-v1", split="validation")
+    ds.save_to_disk("data/wiki")
